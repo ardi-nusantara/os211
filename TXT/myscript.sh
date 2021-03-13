07a2390272178890b51e1b9a6aafe982155f4ba1
Lazuardi Pratama Putra Nusantara <ardinusantara07@gmail.com>
2021-03-13 18:05:15 +0700

    OS211 ardi-nusantara TXT

---
+++ TXT/myscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Cicak bin Kadal
+# Tue 13 Oct 2020 10:37:14 AM WIB
+
+FILES="my*.txt my*.sh"
+SHA="SHA256SUM"
+
+echo "rm -f $SHA $SHA.asc"
+rm -f $SHA $SHA.asc
+
+echo "sha256sum $FILES > $SHA"
+sha256sum $FILES > $SHA
+
+echo "sha256sum -c $SHA"
+sha256sum -c $SHA
+
+echo "gpg -o $SHA.asc -a -sb $SHA"
+gpg -o $SHA.asc -a -sb $SHA
+
+echo "gpg --verify $SHA.asc $SHA"
+gpg --verify $SHA.asc $SHA
+
+exit 0
